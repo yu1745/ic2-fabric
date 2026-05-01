d2f700176fd248e9f144ceaa6340bb8f4a8d861c
wangyu <1658783310@qq.com>
2026-05-01 17:43:26 +0800

    fix: 量子剑创造栏图标改用 iconResource 避免显示耐久条

---
+++ advanced-weapons-addon/src/main/kotlin/ic2_120_advanced_weapons_addon/content/item/CreativeTabIconItems.kt
@@ -0,0 +1,14 @@
+package ic2_120_advanced_weapons_addon.content.item
+
+import net.minecraft.item.Item
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+
+class TabIconIc2AdvancedWeaponsItem : Item(Item.Settings())
+
+object CreativeTabIconItemsRegistration {
+    fun register(modId: String) {
+        Registry.register(Registries.ITEM, Identifier.of(modId, "tab_icon_ic2_advanced_weapons"), TabIconIc2AdvancedWeaponsItem())
+    }
+}
--- advanced-weapons-addon/src/main/kotlin/ic2_120_advanced_weapons_addon/content/tab/ModTabs.kt
+++ advanced-weapons-addon/src/main/kotlin/ic2_120_advanced_weapons_addon/content/tab/ModTabs.kt
@@ -2,5 +2,5 @@ package ic2_120_advanced_weapons_addon.content.tab
 
 import ic2_120.registry.annotation.ModCreativeTab
 
-@ModCreativeTab(name = "ic2_advanced_weapons", iconItem = "quantum_saber")
+@ModCreativeTab(name = "ic2_advanced_weapons", iconResource = "ic2_120_advanced_weapons_addon:item/quantum_saber/inactive")
 class ModTabs
